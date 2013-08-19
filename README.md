104e5a615075c27c4e608cc883ecde452ab904c6
Bob Foster <bobfoster@gmail.com>
2013-08-19 13:30:04 -0700

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,31 @@ delete-team-cli-plugin
 ======================
 
 Adds delete-team and delete-job CLI commands.
+
+delete-team
+===========
+Delete a single team. By default, all jobs in the team are made public. Use the -j
+switch to delete the team jobs instead.
+
+Help:
+    Delete a team
+     TEAM                : Team to delete
+     -j (--jobs)         : Delete jobs (by default jobs are made public)
+
+delete-job
+==========
+Delete one or more jobs whose names match a regex pattern. The pattern is implicitly
+bounded by ^ and $, and must match the entire job name. For example, "A" will delete
+only job A, but ".*A.*" will delete all jobs with a capital A in the name.
+
+The optional TEAM argument restricts deleting to a single team. E.g., to delete
+all jobs in in team MyTeam, <code>delete-job ".*" MyTeam</code>. This is more
+reliable than deleting based on the team prefix in the job name, as '.' (dot) is
+<i>not</i> a reserved character in job names. The may be a job <code>A.Job</code>
+that is not in team A.
+
+Help:
+    Delete one or more jobs
+     JOB                 : Job(s) to delete
+     TEAM                : Only within team (optional)
+
---
+++ src/main/java/org/hudsonci/plugins/team/cli/DeleteTeamCommand.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2013 Hudson.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Hudson - initial API and implementation and/or initial documentation
+ */
+
+package org.hudsonci.plugins.team.cli;
+
+import hudson.Extension;
+import hudson.cli.CLICommand;
+import hudson.model.Hudson;
+import hudson.model.TopLevelItem;
+import java.util.Set;
+import org.eclipse.hudson.security.team.Team;
+import org.eclipse.hudson.security.team.TeamManager;
+import org.eclipse.hudson.security.team.TeamManager.TeamNotFoundException;
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.Option;
+
+/**
+ * Delete a team from the command line. User must be sys admin.
+ * @author Bob Foster
+ */
+@Extension
+public class DeleteTeamCommand extends CLICommand {
+
+    @Override
+    public String getShortDescription() {
+        return "Delete a team";
+    }
+    @Argument(metaVar = "TEAM", usage = "Team to delete", required=true)
+    public String team;
+    @Option(name = "-j", aliases = {"--jobs"}, usage = "Delete jobs (by default jobs are made public)")
+    public boolean jobs;
+
+    protected int run() throws Exception {
+        Hudson h = Hudson.getInstance();
+        
+        if (!h.isTeamManagementEnabled()) {
+            stderr.println("Team management is not enabled");
+            return -1;
+        }
+        
+        TeamManager teamManager = h.getTeamManager();
+        
+        if (!teamManager.isCurrentUserSysAdmin()) {
+            stderr.println("User not authorized to create team");
+            return -1;
+        }
+        
+        Team targetTeam;
+        try {
+            targetTeam = teamManager.findTeam(team);
+        } catch (TeamNotFoundException e) {
+            stderr.println("Team "+team+" not found");
+            return -1;
+        }
+        
+        if (jobs) {
+            Set<String> jobNames = targetTeam.getJobNames();
+            for (String job : jobNames) {
+                TopLevelItem item = h.getItem(job);
+                item.delete();
+            }
+        }
+        
+        teamManager.deleteTeam(team);
+        
+        return 0;
+    }
+}
